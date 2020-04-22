895622403f1ab178bb2bb468d25d3689c8b86684
Davide <davide.roberti89@gmail.com>
2020-04-22 15:23:24 +0200

    metodo con script nell'index e dati passati tramite database

--- index.php
+++ index.php
@@ -8,10 +8,32 @@
         <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     </head>
     <body>
+
         <div class="container">
             <canvas id="grafico-linea"></canvas>
         </div>
 
-        <script src="js/main.js" charset="utf-8"></script>
+        <?php include 'database.php'; ?>;
+
+        <script>
+
+            var data = <?php echo json_encode($data); ?>;
+            var ctx = $('#grafico-linea');
+            var chart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
+                    datasets: [{
+                        label: 'Vendite',
+                        backgroundColor: 'pink',
+                        borderColor: 'blue',
+                        lineTension: 0.3,
+                        data: data
+                    }]
+                }
+            });
+        </script>
+
+        <!-- <script src="js/main.js" charset="utf-8"></script> -->
     </body>
 </html>
