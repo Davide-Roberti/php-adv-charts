e7db0bc802dfe74c44d818d8039d7c5729a441da
Davide <davide.roberti89@gmail.com>
2020-04-22 14:57:29 +0200

    metodo più semplice con javascript

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+var dati = [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322];
+
+var ctx = $('#grafico-linea');
+var chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
+        datasets: [{
+            label: 'Vendite',
+            backgroundColor: 'pink',
+            borderColor: 'blue',
+            lineTension: 0.3,
+            data: dati
+        }]
+    }
+});
