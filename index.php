45e82742c4d7390ae2de5bc42fe16ae75f868214
Davide <davide.roberti89@gmail.com>
2020-04-25 10:51:37 +0200

    aggiornamento con inizio milestone 3, da sistemare grafico linea e torta

--- index.php
+++ index.php
@@ -2,24 +2,51 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="css/style.css">
         <title>adc-charts</title>
+        <link rel="stylesheet" href="css/stile.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     </head>
     <body>
 
-        <div class="container">
-            <canvas id="grafico-linea"></canvas>
-        </div>
 
         <div class="container">
-            <canvas id="secondo-grafico-linea"></canvas>
+            <div class="milestone-1">
+                <h2>Milestone 1</h2>
+                <canvas id="grafico-linea"></canvas>
+            </div>
+        </div>
+        <div class="milestone-2">
+            <h2>Milestone 2</h2>
+            <div class="container">
+                <canvas id="secondo-grafico-linea"></canvas>
+            </div>
+
+            <div class="container">
+                <canvas id="grafico-torta"></canvas>
+            </div>
+        </div>
+        <div class="milestone-3">
+            <h2>Milestone 3</h2>
+            <?php $level = $_GET['level']; ?>
+
+            <?php if ($level == 'guest' || $level == 'employee' || $level == 'clevel') { ?>
+                <div class="container">
+                    <canvas id="secondo-grafico-linea"></canvas>
+                </div>
+            <?php } ?>
+            <?php if ($level == 'employee' || $level == 'clevel') { ?>
+                <div class="container">
+                    <canvas id="grafico-torta"></canvas>
+                </div>
+            <?php } ?>
+            <?php if ($level == 'clevel') { ?>
+                <div class="container">
+                    <canvas id="grafico-multilinea"></canvas>
+                </div>
+            <?php } ?>
         </div>
 
-        <div class="container">
-            <canvas id="grafico-torta"></canvas>
-        </div>
 
         <?php include 'database.php'; ?>;
 
@@ -42,6 +69,6 @@
             });
         </script> -->
 
-        <script src="js/main.js" charset="utf-8"></script>
+        <script src="js/script.js" charset="utf-8"></script>
     </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -79,6 +79,8 @@ function graficoTorta (dataInput) {
 
     var nomiVenditori = Object.keys(dataInput.fatturato_by_agent.data);
     var datiVenditori = Object.values(dataInput.fatturato_by_agent.data);
+    // console.log(nomiVenditori);
+    // console.log(datiVenditori);
 
     var ctx = $('#grafico-torta');
     new Chart(ctx, {
@@ -90,5 +92,56 @@ function graficoTorta (dataInput) {
                 backgroundColor: ['lightblue', 'green', 'pink', 'lightcoral']
             }]
         },
+    });
+}
+
+//milestone 3
+
+$.ajax({
+    url: 'server3.php',
+    method: 'GET',
+    success: function (data) {
+        graficoLinea(data);
+        graficoTorta(data);
+
+        var objectEff = data.team_efficiency.data;
+        var team = [];
+        var efficenza = [];
+        for (var key in objectEff) {
+            team.push(key);
+            efficenza.push(objectEff[key]);
+        }
+        graficoTeam(team[0], team[1], team[2], efficenza[0], efficenza[1], efficenza[2]);
+    },
+    error: function () {
+        alert('errorissimo')
+    }
+});
+
+function graficoTeam (label1, label2, label3, efficenza1, efficenza2, efficenza3) {
+    var ctx = $('#grafico-multilinea');
+    var chart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
+            datasets:
+            [
+                {
+                label: label1,
+                borderColor: 'pink',
+                data: efficenza1,
+                },
+                {
+                label: label2,
+                borderColor: 'blue',
+                data: efficenza2,
+                },
+                {
+                label: label3,
+                borderColor: 'green',
+                data: efficenza3,
+                }
+            ]
+        }
     });
 }
